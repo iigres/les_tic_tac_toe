6408c60d232485cf49317e99b743ce8d8b1d4b9b
sdplion <sdplion@outlook.com>
2020-10-21 18:35:39 +0300

    Updates

---
+++ precache-manifest.785fc2a732115bedde270919ff9febb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2475e572a00dfdac47f62752656c5fc",
+    "url": "/les_tic_tac_toe/index.html"
+  },
+  {
+    "revision": "1fb1187fe294fb02912f",
+    "url": "/les_tic_tac_toe/static/css/main.2a34271c.chunk.css"
+  },
+  {
+    "revision": "4c3dcf5c8c00c9ef43a0",
+    "url": "/les_tic_tac_toe/static/js/2.23a4161f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/les_tic_tac_toe/static/js/2.23a4161f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb1187fe294fb02912f",
+    "url": "/les_tic_tac_toe/static/js/main.2e1ade03.chunk.js"
+  },
+  {
+    "revision": "0c4e973103f8451cb60b",
+    "url": "/les_tic_tac_toe/static/js/runtime-main.c27d1550.js"
+  }
+]);
